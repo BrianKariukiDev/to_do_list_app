ef7ceee291a9b5d25e301ca1e0f6cf2a4c63afdd
Brian Kariuki <kariukibrian087@gmail.com>
2024-09-19 16:22:09 +0300

    updates

--- src/Todolist/ToDoList.jsx
+++ src/Todolist/ToDoList.jsx
@@ -3,23 +3,39 @@ import { useState } from "react"
 const ToDoList = () => {
 
 
-    const [tasks,setTasks] =useState(['Take a shower','feed the chicken']);
+    const [tasks,setTasks] =useState(["Take a shower","feed the chicken"]);
     const [newTask,setNewTasks]= useState("");
 
     function handleInputChange(event){
-        setNewTasks(event.target.vaalue);
+        setNewTasks(event.target.value);
     }
 
     function addTask(){
         if(newTask.trim() !== ""){
-            setTasks(t =>[...tasks,newTask]);
+            setTasks(t =>[...t,newTask]);
             setNewTasks("");
         }
     }
 
     function deleteTask(index){
-        const updatedTasks= tasks.filter((_, i)=> i== index);
+        const updatedTasks= tasks.filter((_, i)=> i !== index);
         setTasks(updatedTasks);
+    }
+
+    function moveUp(index){
+        if(index>0){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index],updatedTasks[index-1]]=[updatedTasks[index-1],updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
+
+    function moveDown(index){
+        if(index<tasks.length-1){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index],updatedTasks[index+1]]=[updatedTasks[index+1],updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
   return (
@@ -37,9 +53,9 @@ const ToDoList = () => {
                     {tasks.map((task, index) => (
                         <li key={index}>
                             <p>{task}</p>
-                            <button>DELETE</button>
-                            <button>☝️</button>
-                            <button>👇</button>
+                            <button className="delete-button" onClick={()=>deleteTask(index)}>DELETE</button>
+                            <button className="move-button" onClick={()=>moveUp(index)}>☝️</button>
+                            <button className="move-button" onClick={()=>moveDown(index)}>👇</button>
                         </li>
                     ))};
                 </ol>
